8e68ed4938fc35c0363882d93458ec0d03c93194
yongli0510 <17302010031@fudan.edu.cn>
2020-05-17 22:19:55 +0800

    增加学生、教师的个人信息页面

--- web-pbl/src/app/pages/teacher/teacher.module.ts
+++ web-pbl/src/app/pages/teacher/teacher.module.ts
@@ -4,32 +4,35 @@ import { CommonModule } from '@angular/common';
 import { TeacherRoutingModule } from './teacher-routing.module';
 import { TeacherMainComponent } from './teacher-main/teacher-main.component';
 import {
-  NzBreadCrumbModule,
-  NzButtonModule,
-  NzIconModule,
-  NzLayoutModule,
-  NzMenuModule,
-  NzToolTipModule
+    NzBreadCrumbModule,
+    NzButtonModule, NzDropDownModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzToolTipModule
 } from "ng-zorro-antd";
 import {AppShareModule} from "../../app-share.module";
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseAddComponent } from './course-add/course-add.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectAddComponent } from './project-add/project-add.component';
+import { InfoComponent } from './info/info.component';
+import { ModifyInfoComponent } from './modify-info/modify-info.component';
 
 
 @NgModule({
-  declarations: [TeacherMainComponent, CourseListComponent, CourseAddComponent, ProjectListComponent, ProjectAddComponent],
-  imports: [
-    CommonModule,
-    TeacherRoutingModule,
-    NzLayoutModule,
-    AppShareModule,
-    NzButtonModule,
-    NzToolTipModule,
-    NzIconModule,
-    NzMenuModule,
-    NzBreadCrumbModule
-  ]
+  declarations: [TeacherMainComponent, CourseListComponent, CourseAddComponent, ProjectListComponent, ProjectAddComponent, InfoComponent, ModifyInfoComponent],
+    imports: [
+        CommonModule,
+        TeacherRoutingModule,
+        NzLayoutModule,
+        AppShareModule,
+        NzButtonModule,
+        NzToolTipModule,
+        NzIconModule,
+        NzMenuModule,
+        NzBreadCrumbModule,
+        NzDropDownModule
+    ]
 })
 export class TeacherModule { }
