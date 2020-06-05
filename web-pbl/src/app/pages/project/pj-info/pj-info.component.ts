cb6b8dd2e8df5aaefa0a5050562d82565bc2a585
Jedar <384034509@qq.com>
2020-06-05 14:28:08 +0800

    fix project info

--- web-pbl/src/app/pages/project/pj-info/pj-info.component.ts
+++ web-pbl/src/app/pages/project/pj-info/pj-info.component.ts
@@ -2,9 +2,11 @@ import {Component, OnInit} from '@angular/core';
 
 import {ProjectService} from "../../../services/project.service";
 import {StudentService} from "../../../services/student.service";
+import {TaskService} from "../../../services/task.service";
 import {ActivatedRoute} from "@angular/router";
 import {Project} from "../../../share/project.model";
 import {Student} from "../../../share/student.model";
+import { Task } from 'src/app/share/task.model';
 
 @Component({
   selector: 'app-pj-info',
@@ -21,9 +23,12 @@ export class PjInfoComponent implements OnInit {
     private projectService: ProjectService,
     private activatedRoute: ActivatedRoute,
     private studentService: StudentService,
+    private taskService: TaskService
   ) {
     activatedRoute.params.subscribe(params => {
-      this.projectId = params['projectId'];
+      // this.projectId = params['projectId'];
+      this.projectId = taskService.getProjectId();
+      console.log(this.projectId);
     });
   }
 
@@ -31,6 +36,7 @@ export class PjInfoComponent implements OnInit {
     this.projectService.getProjectOf(this.projectId).subscribe(result => {
       if(result.code === 200){
          this.project = result.data;
+         console.log(this.project);
       }
       else{
         window.alert("项目信息获取失败");
@@ -40,10 +46,12 @@ export class PjInfoComponent implements OnInit {
 
     this.studentService.getStudentInfo(10009).subscribe(result => {
       this.leader = result.data;
+      console.log(result.message);
     });
 
     this.studentService.getStudentsOfProject(this.projectId).subscribe(result=>{
       this.student = result.data;
+      console.log(result.message);
     })
   }
 
